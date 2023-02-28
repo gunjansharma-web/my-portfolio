8b32fffb73d04287c58bc14afc45147efb7ee64a
Gunjan Sharma <gunjansharma.13dec@gmail.com>
2023-02-28 10:34:03 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+function myFunction() {
+	var x = document.getElementById("hide");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
